f73cb4be55ecbb1dfa64514b5723fe8d43eaf6a9
zhangyong258 <602252708@qq.com>
2022-05-18 18:36:25 +0800

    update request

--- src/request.js
+++ src/request.js
@@ -1,11 +1,23 @@
 import axios from 'axios'
-import QS from 'qs'
+// import QS from 'qs'
 // import router from '@/router'
 // import { rewriteMessage } from '@/utils/message'
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
-  timeout: 30000
+  timeout: 30000,
+  headers: {
+    // 'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Content-Type': 'application/json'
+    /**
+     * Content-Type: application/json ：
+     * 请求体中的数据会以json字符串的形式发送到后端
+     * Content-Type: application/x-www-form-urlencoded：
+     * 请求体中的数据会以普通表单形式（键值对）发送到后端
+     * Content-Type: multipart/form-data：
+     * 它会将请求体的数据处理为一条消息，以标签为单元，用分隔符分开。既可以上传键值对，也可以上传文件。
+     */
+  }
 })
 
 // 请求拦截器
@@ -38,7 +50,7 @@ export function get(url, data = {}) {
 // post请求
 export function post(url, data = {}) {
   return new Promise((resolve, reject) => {
-    instance.post(url, QS.stringify(data))
+    instance.post(url, data)
       .then((response) => {
           resolve(response.data)
         },
